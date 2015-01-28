2c5563bba9e611b2ee93a18f6148d7a47525985c
I1nk <I1nk@users.noreply.github.com>
2015-01-28 12:25:29 -0500

    Create FindCoords.m
    Finds the coords for a xyz in 3 space and rotates it with respect to the x and y axis using the right hand rule.

---
+++ FindCoords.m
@@ -0,0 +1,31 @@
+close all
+clear
+clc
+
+an = 0;
+an1 = 0;
+
+x = -cosd(35);
+z = sind(35);
+y = 0;
+
+x1 = cosd(36);
+z1 = sind(36);
+y1 = 0;
+
+xx = [x;y;z];
+yy = [x1;y1;z1];
+
+rots = [cosd(an), 0, sind(an);
+         0, 1, 0;
+         -sind(an), 0, cosd(an)];
+     
+xx = rots * xx
+yy = rots * yy
+
+rots = [1, 0, 0;
+         0, cosd(an1), -sind(an1);
+         sind(an1), 0, cosd(an1)];
+
+xx = rots * xx
+yy = rots * yy
